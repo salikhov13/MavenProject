7f099242921b58cac1775e886193792d38186763
Daniyar Salikhov <daniyar.salikhov@gmail.com>
2021-12-20 06:34:08 +0100

    Added more hello feature

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,5 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Developing hello feature");
+        System.out.println("Local repo is already published to Github");
+        System.out.println("Doing more work");
     }
 }
